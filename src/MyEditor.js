5df6adc4848fbbca4efc9cd1dd6113b1f487863d
pc-magas <pc-magas@users.noreply.github.com>
2017-12-22 14:45:57 +0200

    Upload Callback Somewhat works

--- src/MyEditor.js
+++ src/MyEditor.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { EditorState, convertToRaw, ContentState } from 'draft-js';
+import { EditorState } from 'draft-js';
 import { Editor } from 'react-draft-wysiwyg';
 
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
@@ -16,14 +16,39 @@ class MyEditor extends Component {
       });
     };
     
+    uploadCallback(file) {
+      var reader = new window.FileReader();
+      reader.onloadend= () => { 
+        fetch('http://localhost:9090/image',{
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            name: file.name,
+            data: reader.result,
+          }),
+        })
+        .then((data)=>{
+            console.log('Uploaded Data',data);
+        });
+      }
+      reader.readAsDataURL(file);
+    }
+
     render() {
       const { editorState } = this.state;
+      const config={
+        image: { uploadCallback: this.uploadCallback }
+      }
       return (
           <Editor
               editorState={editorState}
               wrapperClassName="demo-wrapper"
               editorClassName="demo-editor"
               onEditorStateChange={this.onEditorStateChange}
+              toolbar={ config }
            />
       );
     }
